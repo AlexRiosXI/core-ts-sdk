122e545b3887eeba76d8df5ed76436f90321c966
AlexRiosXI <alexriosxi@gmail.com>
2025-07-23 17:50:47 -0600

    update register

--- src/hooks/useMutation.ts
+++ src/hooks/useMutation.ts
@@ -12,11 +12,26 @@ const useMutation =  (mutation: MutationRequest) => {
     type T = z.infer<typeof mutation.schema>
     
     const [data, setData] = useState<T | null>(generateInitialState(mutation.schema) as T)
+    const [errors, setErrors] = useState<z.infer<typeof mutation.schema> | null>(null)
     const [error, setError] = useState<Error | null>(null)
     const [status, setStatus] = useState<number>(0)
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const mutate = async () => {
-        setIsLoading(true)
+        console.log("mutate")
+      
+        const result = mutation.schema.safeParse(data)
+        if (!result.success) {
+            console.log("error")
+            setErrors(result.error.flatten().fieldErrors)
+            return false
+        }else{
+            setErrors(null)
+        }
+        
+      
+        
+
+        return "hola"
         const response = await axiosClient(mutation)
         setData(response.data)
         setStatus(response.status)
@@ -36,18 +51,25 @@ const useMutation =  (mutation: MutationRequest) => {
           const handleChange = (name: keyof T) => (
         e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
       ) => {
-        setData((prev) => ({
+        setData((prev: any) => ({
           ...(prev || {}),
           [name]: e.target.value,
         } as T));
+
+
       };
     
       const register = (name: keyof T) => ({
         name,
         value: data?.[name] ?? "",
         onChange: handleChange(name),
+        isInvalid: !!errors?.[name] ,
+        errorMessage: errors?.[name]?.[0]
       });
     
+
+  
+
     return {
         data,
         error,
@@ -55,7 +77,9 @@ const useMutation =  (mutation: MutationRequest) => {
         mutate,
         reset,
         isLoading,
-        register
+        register,
+        errors
+        
     }
 }
 
